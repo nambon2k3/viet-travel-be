92544ddf2ab7e5827a08e0fb875bb92b3bcc61c2
nhukhoa <khoa20033002@gmail.com>
2025-02-06 16:12:16 +0700

    delete: delete unnecessary DTO

--- src/main/java/com/fpt/capstone/tourism/service/AuthService.java
+++ src/main/java/com/fpt/capstone/tourism/service/AuthService.java
@@ -3,10 +3,7 @@ package com.fpt.capstone.tourism.service;
 import com.fpt.capstone.tourism.dto.common.TokenDTO;
 import com.fpt.capstone.tourism.dto.common.UserDTO;
 import com.fpt.capstone.tourism.dto.common.GeneralResponse;
-import com.fpt.capstone.tourism.dto.request.RegisterConfirmRequestDTO;
 import com.fpt.capstone.tourism.dto.request.RegisterRequestDTO;
-import com.fpt.capstone.tourism.dto.response.RegisterInforResponseDTO;
-import com.fpt.capstone.tourism.dto.response.RegisterInitResponseDTO;
 import com.fpt.capstone.tourism.dto.response.UserInfoResponseDTO;
 import com.fpt.capstone.tourism.exception.common.BusinessException;
 
