0c5ebfcef9a108ba41d05d0a13cd496ac0967949
LanThan99 <lananhthan99@gmail.com>
2025-02-07 23:02:21 +0700

    update: change user id data type

--- src/main/java/com/fpt/capstone/tourism/controller/UserManagementController.java
+++ src/main/java/com/fpt/capstone/tourism/controller/UserManagementController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/CEO/users")
+@RequestMapping("/api/v1/CEO/users")
 @RequiredArgsConstructor
 public class UserManagementController {
 
