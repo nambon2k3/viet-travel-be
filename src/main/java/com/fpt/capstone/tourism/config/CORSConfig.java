5930753a33ddc1f4cc26338c536b922ff930ebed
Hoai Nam <nvhnam.work@gmail.com>
2025-02-13 18:28:28 +0700

    update: cors

--- src/main/java/com/fpt/capstone/tourism/config/CORSConfig.java
+++ src/main/java/com/fpt/capstone/tourism/config/CORSConfig.java
@@ -9,7 +9,7 @@ public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Client endpoints
+                .allowedOrigins("http://localhost:4200", "https://croakorder.store") // Client endpoints
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") //  HTTP methods allowed to request
                 .allowedHeaders("*")
                 .allowCredentials(true);
