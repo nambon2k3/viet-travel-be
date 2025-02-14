7a29304f34ba4425b3e63d37c1aa41f26f6d68a8
Nguyen Minh Hao <59317824+haominhnguyen@users.noreply.github.com>
2025-02-14 14:15:44 +0700

    fix: spike temp allows all request origin access this api endpoint

--- src/main/java/com/fpt/capstone/tourism/config/CORSConfig.java
+++ src/main/java/com/fpt/capstone/tourism/config/CORSConfig.java
@@ -9,7 +9,8 @@ public class CORSConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://croakorder.store") // Client endpoints
+                // FIXME: temp allow all request from origins
+                .allowedOrigins("http://localhost:4200", "https://croakorder.store", "*") // Client endpoints
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") //  HTTP methods allowed to request
                 .allowedHeaders("*")
                 .allowCredentials(true);
